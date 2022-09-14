48e9b4224a5231f8ac7a5ee7a0470ce1ca017891
manuarg123 <manu571@gmail.com>
2022-09-14 19:10:29 -0300

    Avances dashboar

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_dashboard")
+     */
+    public function index(): Response
+    {
+        return $this->render('dashboard/index.html.twig', [
+            'welcome_message' => 'Bienvenido/a a este e-commerce',
+        ]);
+    }
+}
